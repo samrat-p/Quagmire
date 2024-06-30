ee0a64a165ab83bd2be48717145b006c8e3902ce
samrat-p <mitra.strak.68@gmail.com>
2024-07-01 00:32:16 +0530

    refactor: Remove unused configuration files and components

--- frontend-prod/src/sections/sticky.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-export const Sticky = () => {
-    return (
-        <div className="fixed top-0 left-0 w-full h-12 bg-purpule text-sm text-white text-center flex items-center justify-center z-10">
-          <span ><b>Quagmire Beta</b> is not ready yet! This is just a Toy Version of it. <span className="px-1 text-yellow-400">Learn More</span></span>
-        </div>
-    )
-} 
