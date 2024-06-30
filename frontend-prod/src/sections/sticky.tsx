3c55f110ea5a1b6d16a80c80a0537b983170ffa9
samrat-p <mitra.strak.68@gmail.com>
2024-06-30 23:56:09 +0530

    Add sticky message component

---
+++ frontend-prod/src/sections/sticky.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+export const Sticky = () => {
+    return (
+        <div className="fixed top-0 left-0 w-full h-12 bg-purpule text-sm text-white text-center flex items-center justify-center z-10">
+          <span ><b>Quagmire Beta</b> is not ready yet! This is just a Toy Version of it. <span className="px-1 text-yellow-400">Learn More</span></span>
+        </div>
+    )
+} 
